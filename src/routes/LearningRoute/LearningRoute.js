e0f58a3527a6d3c3293a4b3062a3fb29e5d0da29
Scott Schroeder <snschroeder89@gmail.com>
2019-11-21 16:25:42 -0600

    linked up post with back end, create results page

--- src/routes/LearningRoute/LearningRoute.js
+++ src/routes/LearningRoute/LearningRoute.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import config from '../../config'
 import TokenService from '../../services/token-service'
+import Results from './Results';
 import './LearningRoute.css'
 
 
@@ -11,6 +12,12 @@ class LearningRoute extends Component {
     totalScore: 0,
     wordCorrectCount: 0,
     wordIncorrectCount: 0,
+    qAnswered: false,
+    correct: null,
+    resAnswer: null,
+    upcomingWord: null,
+    resCorrectCount: null,
+    resIncorrectCount: null,
   }
 
   componentDidMount() {
@@ -34,31 +41,41 @@ class LearningRoute extends Component {
   }
   handleAnswer = (e) => {
     e.preventDefault()
+    const { answer } = this.state;
     console.log('in handleAnswer')
-    fetch(`${config.API_ENDPOINT}/language/head`, {
+    fetch(`${config.API_ENDPOINT}/language/guess`, {
+      method: 'POST',
       headers: {
+        'content-type': 'application/json',
         'Authorization': `bearer ${TokenService.getAuthToken()}`
-      }
+      },
+      body: JSON.stringify({
+        guess: answer
+      })
     })
       .then(res =>
         (!res.ok) ? res.json().then(e => Promise.reject(e))
           : res.json()
-      )
-      .then(result => {
-        console.log(result);
-        this.setState(result)
+      ).then(result => {
+        this.setState({
+          qAnswered: true,
+          correct: result.isCorrect,
+          resAnswer: result.answer,
+          upcomingWord: result.nextWord,
+          resCorrectCount: result.wordCorrectWordCount,
+          resIncorrectCount: result.wordIncorrectWordCount,
+          totalScore: result.totalScore,
+        })
       })
   }
 
-
-  render() {
+  displayQuestion = () => {
     const { nextWord, wordCorrectCount, wordIncorrectCount, total_score } = this.state
-
     return (
       <section className='learn-main container'>
-        <h2 className="cypress" style={{ display: 'none'}}>Translate the word:</h2>
+        <h2 className="cypress" style={{ display: 'none' }}>Translate the word:</h2>
         <h3 className="learn-main-h2">What is the name of this algorithm?</h3>
-        <span style={{ display: 'none'}}>{this.state.nextWord}</span>
+        <span style={{ display: 'none' }}>{this.state.nextWord}</span>
         <p className="learn-main-total">Your total score is: 999{total_score}</p>
         <div className='test-main'>
           <p className="question">What is the name of this algorithm?</p>
@@ -72,6 +89,35 @@ class LearningRoute extends Component {
           <button type="submit" onClick={this.handleAnswer}>Submit your answer</button>
         </form>
       </section>
+    )
+  }
+
+
+  render() {
+    const {
+      answer,
+      qAnswered,
+      correct,
+      resAnswer,
+      upcomingWord,
+      resCorrectCount,
+      resIncorrectCount,
+      totalScore
+    } = this.state;
+
+    return (
+      <>
+        {qAnswered
+          ? <Results
+            correct={correct}
+            nextWord={upcomingWord}
+            answer={resAnswer}
+            guess={answer}
+            totalScore={totalScore}
+          />
+          : this.displayQuestion()
+        }
+      </>
     );
   }
 }
---
+++ src/routes/LearningRoute/Results.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import './Results.css';
+
+export default function Results(props) {
+  const {
+    correct,
+    nextWord,
+    answer,
+    guess,
+    totalScore
+  } = props;
+
+  const displayCorrect = () => {
+    return (
+      <section className="results">
+        <div className="shadow">
+          <h2 className="cypress" style={{ display: "none" }}>You were correct! :D</h2>
+          <h3 className="results-h3">You were correct!</h3>
+          <p className="display-score-p">Your total score is: {totalScore}</p>
+          <p className="display-feedback-p">
+            The correct translation for {nextWord} was {answer} and you chose {guess}!
+        </p>
+        </div>
+        <button className="next-button" type="button" onClick={() => { }}>Try another word!</button>
+      </section >
+    )
+  }
+
+  const displayIncorrect = () => {
+    return (
+      <section className="results">
+        <div className="shadow">
+          <h2 className="cypress" style={{ display: "none" }}>Good try, but not quite right :(</h2>
+          <h3 className="results-h3">Good try, but not quite right</h3>
+          <p className="display-score-p">Your total score is: {totalScore}</p>
+          <p className="display-feedback-p">
+            The correct translation for {nextWord} was {answer} and you chose {guess}!
+        </p>
+        </div>
+        <button className="next-button" type="button" onClick={() => { }}>Try another word!</button>
+      </section>
+    )
+  }
+
+  return (
+    correct ? displayCorrect() : displayIncorrect()
+  )
+}
