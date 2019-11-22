c27c867cbd94d524d723b41932340984be3d359c
Scott Schroeder <snschroeder89@gmail.com>
2019-11-22 11:19:23 -0600

    updated styling

--- src/routes/LearningRoute/Results.jsx
+++ src/routes/LearningRoute/Results.jsx
@@ -18,7 +18,8 @@ export default function Results(props) {
           <h2 className="cypress" style={{ display: "none" }}>You were correct! :D</h2>
           <h3 className="results-h3">You were correct!</h3>
           <p className="display-score-p">Your total score is: {totalScore}</p>
-          <p className="display-feedback-p">The correct translation for</p>
+          <p className="display-feedback-p" style={{ display: 'none' }}>The correct translation for</p>
+          <h4 className="display-feedback-h4">The correct name for this algorithm</h4>
           <img className="results-img" src={`./imgAssets/${nextWord}`} alt='algorithm question' />
           <p className="display-feedback-p">was {answer} and you chose {guess}!</p>
         </div>
@@ -34,7 +35,8 @@ export default function Results(props) {
           <h2 className="cypress" style={{ display: "none" }}>Good try, but not quite right :(</h2>
           <h3 className="results-h3">Good try, but not quite right</h3>
           <p className="display-score-p">Your total score is: {totalScore}</p>
-          <p className="display-feedback-p">The correct translation for</p>
+          <p className="display-feedback-p" style={{ display: 'none' }}>The correct translation for</p>
+          <h4 className="display-feedback-h4">The correct name for this algorithm</h4>
           <img className="results-img" src={`./imgAssets/${nextWord}`} alt='algorithm question' />
           <p className="display-feedback-p">was {answer} and you chose {guess}!</p>
         </div>
